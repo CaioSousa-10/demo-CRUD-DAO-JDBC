b84d9613d9b9b22160498ea7bfb4cbd9052530e2
Caio Vinicius <caioviniciussousadema@gmail.com>
2023-02-15 15:48:22 -0300

    Created SellerDaoJDBC and DaoFactory

--- src/aplication/DemoDaoJdbc.java
+++ src/aplication/DemoDaoJdbc.java
@@ -2,6 +2,8 @@
 package aplication;
 
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,7 @@ public class DemoDaoJdbc {
     public static void main(String[] args) {
         Department obj = new Department(2, "Books");
         Seller seller  = new Seller(1, "Caio", "caio@gmail.com", new Date(), 3000.00, obj);
+        SellerDao sellerdao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
